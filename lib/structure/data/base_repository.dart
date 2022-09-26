410c6dca0fc4872818134da0bd113d2d9e2ff055
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-09-26 12:12:05 +0800

    remove base api dependency

--- lib/structure/data/base_repository.dart
+++ lib/structure/data/base_repository.dart
@@ -1,7 +1,1 @@
-import 'package:flutter_deriv_api/services/connection/api_manager/base_api.dart';
-
-abstract class BaseRepository {
-  BaseRepository(this.api);
-
-  final BaseAPI? api;
-}
+abstract class BaseRepository {}
