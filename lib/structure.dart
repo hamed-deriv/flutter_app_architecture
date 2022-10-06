9ed09c6dbed9d3487f21fa5c105141bcdafb6a42
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-10-06 12:29:19 +0800

    add export file

---
+++ lib/structure.dart
@@ -0,0 +1,7 @@
+export './structure/domain/base_entity.dart';
+export './structure/domain/base_repository.dart';
+export './structure/domain/base_service.dart';
+export './structure/presentation/base_widget.dart';
+export './structure/presentation/state_manager/base_cubit.dart';
+export './structure/presentation/state_manager/base_state.dart';
+export './structure/presentation/state_manager/base_state_status.dart';
