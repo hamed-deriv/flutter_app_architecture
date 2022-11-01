11c67061496e73981f26552d4bf73912ce79e5a6
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-11-01 14:06:55 +0800

    add mapper class

--- lib/structure/domain/base_repository.dart
+++ lib/structure/domain/base_repository.dart
@@ -1,1 +1,7 @@
-abstract class BaseRepository {}
+import 'package:flutter_app_architecture/structure/data/base_mapper.dart';
+
+abstract class BaseRepository<E, M> {
+  BaseRepository(this.mapper);
+
+  final BaseMapper<E, M> mapper;
+}
