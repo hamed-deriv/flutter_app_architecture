d070127c9aa5fce062754fb459266db61e82cb87
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-11-02 13:34:55 +0800

    add mapper to data layer

--- lib/structure/data/base_mapper.dart
+++ lib/structure/data/base_mapper.dart
@@ -1,5 +1,8 @@
-abstract class BaseMapper<E, M> {
-  M fromEntity(E entity);
+import 'package:flutter_app_architecture/components.dart';
+import 'package:flutter_app_architecture/structure/data/base_model.dart';
 
-  E toEntity(M model);
+abstract class BaseMapper<E extends BaseEntity, Model extends BaseModel> {
+  Model fromEntity(E entity);
+
+  E toEntity(Model model);
 }
