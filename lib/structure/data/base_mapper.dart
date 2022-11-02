0d179e392ce633376ad7b8090d9c6b0ac91005cb
hamed-deriv <57184669+hamed-deriv@users.noreply.github.com>
2022-11-02 14:52:15 +0800

    update component class

--- lib/structure/data/base_mapper.dart
+++ lib/structure/data/base_mapper.dart
@@ -1,5 +1,4 @@
 import 'package:flutter_app_architecture/components.dart';
-import 'package:flutter_app_architecture/structure/data/base_model.dart';
 
 abstract class BaseMapper<E extends BaseEntity, Model extends BaseModel> {
   Model fromEntity(E entity);
